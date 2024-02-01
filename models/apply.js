3f170ddeff1fb9ebee3cbccb48320ab90240d7c9
dinmoy <imhyojin614@gmail.com>
2024-02-01 17:40:16 +0900

    [Feat] apply model 생성

---
+++ models/apply.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize')
+
+const Apply = (sequelize) => {
+    return sequelize.define('apply_form', {
+        id: {
+            type: DataTypes.INTEGER,
+            PrimaryKey: true,
+            AutoIncrement: true,
+            allowNull: false,
+        },
+        club_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            reference: {
+                model: 'club',
+                key: 'id',
+            },
+        },
+        name: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        student_num: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        phone_num: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        question1: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        question2: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        question3: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+    });
+};
+module.exports = Apply;
