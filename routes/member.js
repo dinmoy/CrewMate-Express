6318b427089dfa7c914fc99216eb6dd8bde6c509
dinmoy <imhyojin614@gmail.com>
2024-02-02 19:23:07 +0900

    [Feat] reading all members

--- routes/member.js
+++ routes/member.js
@@ -3,6 +3,18 @@ const { Member } = require('../models')
 
 const router = express.Router()
 
+//모든 부원 조회
+router.get('/',async(req,res)=>{
+    try{
+        const members=await Member.findAll()
+        return res.status(201).json(members)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:'Error reading all members'})
+    }
+})
+
+//부원 생성
 router.post('/',async(req,res)=>{
     try{
         const {club_id,name,role,introduction,profile}=req.body
