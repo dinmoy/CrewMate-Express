3671be2c8767a80791e762aa3d0cb62f9a69e3a4
dinmoy <imhyojin614@gmail.com>
2024-02-02 20:38:06 +0900

    [Feat] upadting apply-form api

--- routes/apply.js
+++ routes/apply.js
@@ -4,6 +4,7 @@ const {Apply}=require('../models')
 
 const router=express.Router()
 
+//지원폼 생성
 router.post('/',async(req,res)=>{
     try{
         const {club_id,question1,question2,question3}=req.body
@@ -12,6 +13,19 @@ router.post('/',async(req,res)=>{
     }catch(err){
         console.log(err)
         return res.status(500).json({error:'Error creating applyform'})
+    }
+})
+
+router.put('/:id',async(req,res)=>{
+    const formId=req.params.id
+    try{
+        const form=await Apply.findByPk(formId)
+        if(!form) return res.status(404).json('applyForm not found')
+        await form.update(req.body)
+        return res.status(200).json(form)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:'Error updating'})
     }
 })
 module.exports=router
