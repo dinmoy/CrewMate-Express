252f8bc373f3864aa67867cb86800ce79253adba
dinmoy <imhyojin614@gmail.com>
2024-02-02 15:08:48 +0900

    [Feat] sign up & sign in api

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,11 @@ const {User}=require('../models')
 
 const router=express.Router()
 
-router.get('/signup',async(req,res)=>{
+router.post('/signup',async(req,res)=>{
     try{
-        const {name,email,password,created_date}=req.body
+        const {name,email,password}=req.body
         const newUser=await User.create({
-            ...req,body
+            ...req.body
         })
         return res.status(201).json(newUser)
     }catch(err){
@@ -16,4 +16,23 @@ router.get('/signup',async(req,res)=>{
     }
 });
 
+router.post('/login',async(req,res)=>{
+    try{
+        const {email,password}=req.body
+
+        const user=await User.findOne({where: {email}})
+
+        if(user){
+            const passwordMatch=await bcrypt.compare(password,user.password)
+
+            if(passwordMatch){
+                return res.status(200).json(user);
+            }
+        }
+        return res.status(401).json({error: 'Invalid credentials'})
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error : 'Error login'})
+    }
+})
 module.exports=router
