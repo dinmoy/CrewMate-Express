ec5efc5415e3efd74d767d485706935a6d027af0
dinmoy <imhyojin614@gmail.com>
2024-02-02 19:31:32 +0900

    [Feat] deleting member api

--- routes/member.js
+++ routes/member.js
@@ -40,4 +40,18 @@ router.put('/:id',async(req,res)=>{
         return res.status(500).json({error:'Error updating member'})
     }
 })
+
+//부원 삭제
+router.delete('/:id',async(req,res)=>{
+    const memberId=req.params.id
+    try{
+        const member=await Member.findByPk(memberId)
+        if(!member) return res.status(404).json('Member not found')
+        await member.destroy()
+        return res.status(204).json()
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:'Erro deleting member'})
+    }
+})
 module.exports=router
