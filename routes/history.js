545557ac044e9e7cec9cf1fc3f52c3d777dfcd39
dinmoy <imhyojin614@gmail.com>
2024-02-03 23:53:41 +0900

    [Feat] create history api

--- routes/history.js
+++ routes/history.js
@@ -12,4 +12,15 @@ router.get('/',async(req,res)=>{
         return res.status(500).json({error:'Error reading all histories'})
     }
 })
+
+router.post('/',async(req,res)=>{
+    try{
+        const {clud_id,name,date,thumbnail}=req.body
+        const newHistory=await History.create({...req.body})
+        return res.status(201).json(newHistory)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:'Error creating history'})
+    }
+})
 module.exports=router
