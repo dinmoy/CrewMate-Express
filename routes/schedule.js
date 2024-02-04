691b40146c3846f6151e7ae13736e03733edff98
dinmoy <imhyojin614@gmail.com>
2024-02-04 17:59:42 +0900

    [Feat] read all schedule api

--- routes/schedule.js
+++ routes/schedule.js
@@ -3,6 +3,15 @@ const{Schedule}=require('../models')
 
 const router=express.Router()
 
+router.get('/',async(req,res)=>{
+    try{
+        const Schedules=await Schedule.findAll()
+        return res.status(200).json(Schedules)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:'Error reading all schedules'})
+    }
+})
 router.post('/',async(req,res)=>{
     try{
         const {club_id,name,place,startTime,endTime,memo}=req.body
